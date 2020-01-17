ba1c4f57d2e573beb398df41d40c4eb52fb4d833
ela-p-p <elizabeth.porter.pevec@gmail.com>
2020-01-16 20:02:34 -0500

    Updates

--- precache-manifest.31262fa473e3172dd6b6507d51bce380.js
+++ precache-manifest.31262fa473e3172dd6b6507d51bce380.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "269ee9bcfd8926d5e3c5cec133b46dbb",
+    "revision": "9a5e7448a53c20c79e462f49173b323f",
     "url": "/facerecognitionbrain/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/facerecognitionbrain/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "4388d4284fea1ac7839a",
+    "revision": "a8c832e2eddab7f72313",
     "url": "/facerecognitionbrain/static/css/main.6f2151fd.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/facerecognitionbrain/static/js/2.57ba31ba.chunk.js.LICENSE"
   },
   {
-    "revision": "4388d4284fea1ac7839a",
-    "url": "/facerecognitionbrain/static/js/main.8d5d5501.chunk.js"
+    "revision": "a8c832e2eddab7f72313",
+    "url": "/facerecognitionbrain/static/js/main.bae3af7f.chunk.js"
   },
   {
     "revision": "c8d2b0e1fc3a846871ed",
