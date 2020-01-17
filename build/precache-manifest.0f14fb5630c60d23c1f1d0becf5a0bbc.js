78673481ca04a36fb0a89ddd10f0c55bc6ba5278
ela-p-p <elizabeth.porter.pevec@gmail.com>
2020-01-16 19:00:01 -0500

    build

---
+++ build/precache-manifest.0f14fb5630c60d23c1f1d0becf5a0bbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b98f1fbbfc2555eb5bda3f4ffacaae",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "391626265cd54df3b9dc",
+    "url": "/facerecognitionbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0f82df2ce1d07f188694",
+    "url": "/facerecognitionbrain/static/css/main.6f2151fd.chunk.css"
+  },
+  {
+    "revision": "391626265cd54df3b9dc",
+    "url": "/facerecognitionbrain/static/js/2.57ba31ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facerecognitionbrain/static/js/2.57ba31ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f82df2ce1d07f188694",
+    "url": "/facerecognitionbrain/static/js/main.ed37dcb1.chunk.js"
+  },
+  {
+    "revision": "c8d2b0e1fc3a846871ed",
+    "url": "/facerecognitionbrain/static/js/runtime-main.063892f2.js"
+  }
+]);
